647b5ad1a6fc6666ef302169eb1cce99a012e166
HHuangYQ <1135782479@qq.com>
2022-07-04 21:16:50 +0800

    deploy

---
+++ precache-manifest.720d9a4c6d2cfce701a907d63e32630f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769583ddda266f0c3ad2",
+    "url": "/morney-1-website/css/app.c23b0922.css"
+  },
+  {
+    "revision": "5ba9e1a2d921f3fe94ab2c0d8e507247",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "769583ddda266f0c3ad2",
+    "url": "/morney-1-website/js/app.716b058a.js"
+  },
+  {
+    "revision": "e0c123db2108e7f95729",
+    "url": "/morney-1-website/js/chunk-vendors.30dbc1e6.js"
+  },
+  {
+    "revision": "38c86ee580c0f0227a68bad3d0663039",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
